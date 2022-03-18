a00e2c6bc6bfa91eaadb4ec1b126ed89913b449e
Gerardo Morales <geramoralesrico+code@gmail.com>
2022-03-18 17:36:38 -0600

    Created Context for the app

--- src/components/users/UserResults.js
+++ src/components/users/UserResults.js
@@ -1,29 +1,31 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useContext } from 'react'
 import Spinner from '../layout/Spinner'
 import UserItem from './UserItem'
+import GithubContext from '../../context/github/GithubContext'
 
 const UserResults = () => {
-  const [users, setUsers] = useState([])
-  const [isLoading, setIsLoading] = useState(true)
+  const { users, isLoading, fetchUsers } = useContext(GithubContext)
+  // const [users, setUsers] = useState([])
+  // const [isLoading, setIsLoading] = useState(true)
 
   useEffect(() => {
     fetchUsers()
   }, [])
 
-  const fetchUsers = async () => {
-    // console.log('running....')
-    const URL = `${process.env.REACT_APP_GITHUB_URL}/users`
+  // const fetchUsers = async () => {
+  //   // console.log('running....')
+  //   const URL = `${process.env.REACT_APP_GITHUB_URL}/users`
 
-    const response = await fetch(URL, {
-      headers: {
-        Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`,
-      },
-    })
-    const data = await response.json()
+  //   const response = await fetch(URL, {
+  //     headers: {
+  //       Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`,
+  //     },
+  //   })
+  //   const data = await response.json()
 
-    setUsers(data)
-    setIsLoading(false)
-  }
+  //   setUsers(data)
+  //   setIsLoading(false)
+  // }
 
   if (!isLoading) {
     return (
@@ -36,7 +38,7 @@ const UserResults = () => {
           md:grid-cols-2'
       >
         {users.map((user) => (
-          <UserItem key={user.id}  user={user}   />
+          <UserItem key={user.id} user={user} />
           // <h3>{user.login}</h3>
         ))}
       </div>
